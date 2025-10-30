61fd3eb3afcbe3c221207fd6d5dbe8be8827a14f
Grant Lemons <grantlemons@aol.com>
2025-10-29 20:47:35 -0600

    initialize repository

---
+++ src/tests/test_modules.py
@@ -0,0 +1,11 @@
+import unittest
+import subprocess
+from os import path
+
+from gradescope_utils.autograder_utils.decorators import weight, number
+
+class TestBase(unittest.TestCase): 
+    pass
+
+class TestModules(TestBase): 
+    pass
