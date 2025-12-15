93a7f515f93d2772e551976218fd59161745f800
Grant Lemons <grantlemons@aol.com>
2025-12-15 13:49:43 -0700

    rename some methods

--- src/tests/test_modules.py
+++ src/tests/test_modules.py
@@ -9,36 +9,36 @@ class TestBase(unittest.TestCase):
         if res != 0:
             raise AssertionError(f'Unable to run student\'s Jack Analyzer on {name}.jack!')
 
-    def asserDiffMatch(self, dirname, name):
+    def assertDiffMatch(self, dirname, name):
         res = subprocess.call(['diff', f'/autograder/grader/tests/expected-outputs/{dirname}/{name}.xml', f'/autograder/source/{dirname}/{name}.xml', '--strip-trailing-cr'])
         if res != 0:
             diff = subprocess.check_output(['/bin/sh', '-c', f'diff /autograder/grader/tests/expected-outputs/{dirname}/{name}.xml /autograder/source/{dirname}/{name}.xml --strip-trailing-cr ; exit 0'], text=True)
             print(f'Files differ!\n{diff}')
             raise AssertionError(f'Student\'s XML did not match the provided XML file!')
 
-    def assertCorrectTranslator(self, testpath):
+    def assertCorrectAnalyzer(self, testpath):
         dirname, name = testpath.split('/')
         self.runStudentCode(dirname, name)
-        self.asserDiffMatch(dirname, name)
+        self.assertDiffMatch(dirname, name)
         subprocess.run(['mv', f'/autograder/source/{dirname}/{name}.xml', '/autograder/outputs/'])
 
 class TestModules(TestBase): 
     @weight(47.5/3)
     @number(1)
     def test_square_main(self):
-        self.assertCorrectTranslator('Square/Main')
+        self.assertCorrectAnalyzer('Square/Main')
 
     @weight(47.5/3)
     @number(2)
     def test_square_square(self):
-        self.assertCorrectTranslator('Square/Square')
+        self.assertCorrectAnalyzer('Square/Square')
 
     @weight(47.5/3)
     @number(3)
     def test_square_square_game(self):
-        self.assertCorrectTranslator('Square/SquareGame')
+        self.assertCorrectAnalyzer('Square/SquareGame')
 
     @weight(47.5)
     @number(4)
     def test_array_test_main(self):
-        self.assertCorrectTranslator('ArrayTest/Main')
+        self.assertCorrectAnalyzer('ArrayTest/Main')
